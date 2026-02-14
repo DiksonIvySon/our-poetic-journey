b0676e866af8c0d7eff6a85c6b1300a888f66e42
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2026-02-14 13:38:36 +0200

    Setup for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+// Your GitHub repo name
+const repoName = "our-poetic-journey";
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? `/${repoName}/` : "/",
   server: {
     host: "::",
     port: 8080,
@@ -19,3 +23,26 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+// import path from "path";
+// import { componentTagger } from "lovable-tagger";
+
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   server: {
+//     host: "::",
+//     port: 8080,
+//     hmr: {
+//       overlay: false,
+//     },
+//   },
+//   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// }));
